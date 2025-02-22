d4783b9819c01151d445094060f16436a987b476
Alex Lewis <alewis@no10.gov.uk>
2025-02-22 17:11:08 +0000

    empty python walkthrough page

---
+++ app/python/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import {Text} from "@mantine/core"
+
+const PythonPage = () => {
+    return (
+        <div className="flex flex-col m-4 gap-4">
+        <Text>Python walkthrough</Text>
+        </div>
+    )
+}
+
+export default PythonPage;
